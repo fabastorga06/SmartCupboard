c96dfd3be89d1e9044490d1aaf089b35bb619f5d
fabastorga06 <astorgafabian6@gmail.com>
2019-03-28 00:14:36 -0600

    Change login code in order to adapt it according to the mobile screen dimensions.

--- app/App.js
+++
@@ -1,9 +0,0 @@
-import { createStackNavigator } from 'react-navigation';
-
-import Login from './screens/login';
-
-const App = createStackNavigator({
-  Login: { screen: Login }
-})
-
-export default App
